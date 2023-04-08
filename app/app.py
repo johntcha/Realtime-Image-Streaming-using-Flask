1ad901aec383067638528b8de25a4392b4ed8592
John Tcha <johntcha94@gmail.com>
2023-04-09 00:02:41 +0200

    adding buttons front + adding css/scss

--- app/app.py
+++ app/app.py
@@ -1,8 +1,18 @@
 from flask import Flask, Response, render_template
 from camera import generate_camera_stream
+from flask_assets import Environment, Bundle
 
 
 app = Flask(__name__)
+assets = Environment(app)
+# gen/main.css file is the output CSS file
+# libsass tells Flask-Assets to use the libsass library to compile the SCSS code to CSS
+# depends=('**/*.scss') tells Flask-Assets to rebuild the CSS file
+# whenever any .scss file in the project changes
+scss_bundle = Bundle(
+    "scss/main.scss", output="gen/main.css", filters="libsass", depends=("**/*.scss",)
+)
+assets.register("scss_all", scss_bundle)
 
 
 # Routes
